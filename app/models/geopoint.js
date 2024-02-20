e7cacfb1cd01d6d382511ccef94130815fc32693
Andreo Van Onckelen <andreo141mlg@gmail.com>
2024-02-20 16:10:36 +0100

    Added JSONAPI serializer

--- app/models/geopoint.js
+++ app/models/geopoint.js
@@ -2,6 +2,6 @@ import Model, { attr, belongsTo, hasMany } from "@ember-data/model";
 export default class GeopointModel extends Model {
   @attr latitude;
   @attr longitude;
-  @hasMany("road-sign-concepts", { async: false, inverse: null })
+  @hasMany("road-sign-concept", { async: false, inverse: null })
   signs;
 }
